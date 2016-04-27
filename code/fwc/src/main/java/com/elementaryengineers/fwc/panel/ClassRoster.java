cd14845ee07048ef7015007080174e1d778036b1
Olga <suldina@hotmail.com>
2016-04-27 13:58:09 -0400

    Changes to ClassRoster

--- code/fwc/src/main/java/com/elementaryengineers/fwc/panel/ClassRoster.java
+++ code/fwc/src/main/java/com/elementaryengineers/fwc/panel/ClassRoster.java
@@ -3,6 +3,7 @@ package com.elementaryengineers.fwc.panel;
 import com.elementaryengineers.fwc.custom.ImageButton;
 import com.elementaryengineers.fwc.custom.TitleLabel;
 import com.elementaryengineers.fwc.db.FWCConfigurator;
+import com.elementaryengineers.fwc.model.Student;
 import com.elementaryengineers.fwc.model.Worksheet;
 
 import javax.swing.*;
@@ -22,11 +23,11 @@ public class ClassRoster extends JPanel{
 
     private JPanel pnNorth, pnCenter, pnButtons;
     private TitleLabel lblTitle;
-    private ImageButton btnPrint, btnDelete;
+    private ImageButton btnProfile, btnHistory;
     private DefaultTableModel tableModel;
-    private JTable sheetsTable;
+    private JTable rosterTable;
     private JScrollPane tableScroll;
-    private ArrayList<Worksheet> sheets;
+    private ArrayList<Student> students;
 
     public ClassRoster() {
         super(new BorderLayout());
@@ -35,7 +36,7 @@ public class ClassRoster extends JPanel{
         // Build title and north panel
         pnNorth = new JPanel(new FlowLayout(FlowLayout.CENTER));
         pnNorth.setBackground(Color.WHITE);
-        lblTitle = new TitleLabel("Class Roster", FWCConfigurator.CLASS_ROSTER_IMG);
+        lblTitle = new TitleLabel("Class Roster",FWCConfigurator.CLASS_ROSTER_IMG);
         pnNorth.add(lblTitle);
 
         // Build center panel
@@ -47,7 +48,96 @@ public class ClassRoster extends JPanel{
         tableModel.setColumnIdentifiers(new String[]{"Student Name", "Username",
             "Difficulty Level"});
         
+        tableScroll = new JScrollPane(rosterTable, 
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        pnCenter.add(tableScroll, BorderLayout.CENTER);
         
+        
+        // Build south panel
+        pnButtons = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        pnButtons.setBackground(Color.WHITE);
+        btnProfile = new ImageButton("Profile", FWCConfigurator.PROFILE_IMG, 150, 50);
+        btnProfile.addActionListener(new ProfileListener());
+        
+        btnHistory = new ImageButton("History", FWCConfigurator.HISTORY_IMG, 150, 50);
+        btnHistory.addActionListener(new HistoryListener());
+        pnButtons.add(btnProfile);
+        pnButtons.add(btnHistory);
+
+        this.add(pnNorth, BorderLayout.NORTH);
+        this.add(pnCenter, BorderLayout.CENTER);
+        this.add(pnButtons, BorderLayout.SOUTH);
+        
+        
+    }
+        
+        public void populateTable() {
+        // Remove all rows first
+        for (int i = 0, len = tableModel.getRowCount(); i < len; i++) {
+            tableModel.removeRow(i);
+        }
+
+        for (Student student : students) {
+            tableModel.addRow (new String[]{student.getStudent(),
+                    FWCConfigurator.getStudent().get(student.getDifficultyID())
+            });
+        }
+    }
+
+    private class ProfileListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int index = rosterTable.getSelectedRow();
+
+            // Check if selected a student
+            if (index > 0) {
+                student.get(index).print(false);
+            }
+            else { // No profile is choosen
+                JOptionPane.showMessageDialog(null, 
+                    "Please select student's name from the table first.",
+                     "Student Profile Generation Error",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+    }
+
+    private class HistoryListener implements ActionListener {
+        @Override
+      public void actionPerformed(ActionEvent e) {
+        int index = rosterTable.getSelectedRow();
+
+            // Check if selected a student
+        if (index > 0) {
+                // Check database update status
+        if (FWCConfigurator.getDbConn().getHistory(students.getStudentID())) {; // Remove from student history
+            populateTable(); // Refresh table of students
+            JOptionPane.showMessageDialog(null, 
+                    "History has been created successfully!",
+                    "History Worksheets is created successfully", 
+                    JOptionPane.INFORMATION_MESSAGE);
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, 
+                    "History could not be generated! "
+                    + "Please try again later.",
+                   "Creating History Worksheet Failed",
+                   JOptionPane.INFORMATION_MESSAGE);
+                }
+            
+        }
+    }
+
+    public void refresh() { // Reload table for new student login
+        students = FWCConfigurator.getStudent().getHistory();
+        populateTable();
+    }
+}
+    
+        
+    
         
 
-    }
+    
