677e593d862e35736a534339176d8cc41ad1ca73
Olga <suldina@hotmail.com>
2016-04-28 17:59:19 -0400

    Added the header to each class with the name of the project, team name, purpose

--- code/fwc/src/main/java/com/elementaryengineers/fwc/model/Student.java
+++ code/fwc/src/main/java/com/elementaryengineers/fwc/model/Student.java
@@ -1,4 +1,19 @@
 package com.elementaryengineers.fwc.model;
+/****************************************************************************
+ * Name: Fraction Worksheet Creator
+ * Team: Elementary Engineers 
+ * Date produced: 04/28/2016
+ * ________________________________
+ * Purpose of program:
+ * The Fraction Worksheet Creator (FWC) is a new stand-alone product 
+ * that allows teachers and students to create random exercise worksheets 
+ * to practice operations with fractions.The generated worksheets can contain 
+ * fraction problems of various difficulty levels, from basic addition and 
+ * subtraction problems with visuals and images suitable for small children, 
+ * to quite advanced fraction equations. 
+ * ****************************************************************************
+ */
+
 
 import com.elementaryengineers.fwc.db.FWCConfigurator;
 
--- code/fwc/src/main/java/com/elementaryengineers/fwc/random/WorksheetTester.java
+++ code/fwc/src/main/java/com/elementaryengineers/fwc/random/WorksheetTester.java
@@ -1,3 +1,18 @@
+/****************************************************************************
+ * Name: Fraction Worksheet Creator
+ * Team: Elementary Engineers 
+ * Date produced: 04/28/2016
+ * ________________________________
+ * Purpose of program:
+ * The Fraction Worksheet Creator (FWC) is a new stand-alone product 
+ * that allows teachers and students to create random exercise worksheets 
+ * to practice operations with fractions.The generated worksheets can contain 
+ * fraction problems of various difficulty levels, from basic addition and 
+ * subtraction problems with visuals and images suitable for small children, 
+ * to quite advanced fraction equations. 
+ * ****************************************************************************
+ */
+
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 //  Class        :  WorksheetTester
 //  Author       :  Eric Holm
